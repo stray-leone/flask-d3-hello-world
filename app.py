51275ae171594462469f4ae50ed8934057d18947
stray.leone <stray.leone@gmail.com>
2015-01-15 11:40:22 +0900

    [not work] draw the tree graph with dot format.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ This file is part of the flask+d3 Hello World project.
 import json
 import flask
 import numpy as np
+import pydot
 
 
 app = flask.Flask(__name__)
@@ -15,7 +16,24 @@ def index():
     When you request the root path, you'll get the index.html template.
 
     """
-    return flask.render_template("index.html")
+
+    graph = pydot.graph_from_dot_file("dot.dot")
+    d3_graph_source =""
+
+    dot_content = graph.to_string().strip()
+    '''
+    for d in dot_content.split("\n"):
+        print d
+        #raw_input()
+        '''
+    print "loaded"
+    #d3_graph_source = "\\n\t".join(dot_content.split("\n"))
+    d3_graph_source = dot_content.replace("\n","").replace(";","")
+    print [d3_graph_source]
+    #d3_graph_source= 'digraph{ a }'
+    #return flask.render_template("index.html")
+    return flask.render_template("second.html", graph=d3_graph_source)
+    #return flask.render_template("0.html", graph=d3_graph_source)
 
 
 @app.route("/data")
@@ -43,11 +61,12 @@ def data(ndata=100):
 if __name__ == "__main__":
     import os
 
+    host = "0.0.0.0"
     port = 8000
 
     # Open a web browser pointing at the app.
-    os.system("open http://localhost:{0}".format(port))
+    #os.system("open http://localhost:{0}".format(port))
 
     # Set up the development server on port 8000.
     app.debug = True
-    app.run(port=port)
+    app.run(port=port,host=host)
